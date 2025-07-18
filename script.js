dc2086714d577f9360f81caf615fa4a35331f1b8
PtitJaune <vhugo1999@live.fr>
2025-07-18 12:14:54 +0200

    tfh

---
+++ script.js
@@ -0,0 +1,14 @@
+// Creation d'une bibliothèque
+const library = [];
+
+// Fonction qui ajoute un livre dans la bibliothèque
+function addBook(book) {
+
+    library.push(book);
+}
+
+addBook{(
+     
+}
+
+console.log(library);
